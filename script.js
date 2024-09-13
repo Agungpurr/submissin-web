c1d8aba2353f07a915b9f2e980c2d5512d78a92c
Agung Purnomo <122212227+Agungpurr@users.noreply.github.com>
2024-09-13 15:13:02 +0700

    first init github

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hello World 2024");
+alert("Selamat Datang di website kami");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      alert(`Selamat datang di ${this.textContent}!`);
+    });
+  });
+});
